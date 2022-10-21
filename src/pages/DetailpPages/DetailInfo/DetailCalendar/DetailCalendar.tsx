c4453e293c3e969d8821ea20e5cccf74e1377837
Hy Ngo <ngotanhy123456@gamil.com>
2022-10-21 16:40:19 +0700

    add calender

--- src/pages/DetailpPages/DetailInfo/DetailCalendar/DetailCalendar.tsx
+++ src/pages/DetailpPages/DetailInfo/DetailCalendar/DetailCalendar.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import * as React from "react";
+import DateRangePicker, {
+  OnSelectCallbackParam,
+  StateDefinitions,
+} from "react-daterange-picker";
+import Moment from "moment";
+import { extendMoment } from "moment-range";
 
-type Props = {}
+const moment:any = extendMoment(Moment);
 
-export default function DetailCalendar({}: Props) {
+const stateDefinitions: StateDefinitions = {
+  available:{
+    color : 'null',
+    label: "Available",
+  },
+  unavailable: {
+    selectable: false,
+    color: "#78818b",
+    label: "Unavailable",
+  },
+  fancy: {
+    color: "purple",
+    label: "Fancy",
+  },
+};
+
+const dateRanges = [
+  // {
+  //   state: "enquire",
+  //   range: moment.range(
+  //     moment().add(2, "weeks").subtract(5, "days"),
+  //     moment().add(2, "weeks").add(6, "days")
+  //   ),
+  // },
+  {
+    state: "fancy",
+    range: moment.range(moment().add(1, "days"), moment().add(3, "days")),
+  },
+  {
+    state: "unavailable",
+    range: moment.range(
+      moment().add(3, "weeks"),
+      moment().add(3, "weeks").add(5, "days")
+    ),
+  },
+];
+
+export const DetailCalendar: React.FC<{}> = () => {
+  const [value, setValue] = React.useState();
+
+  const handleSelect=(value: OnSelectCallbackParam)=>{
+    setValue(moment.range(value.start, value.end));
+  }
+
   return (
-    <div>DetailCalendar</div>
-  )
-}
+    <DateRangePicker
+      firstOfWeek={1}
+      numberOfCalendars={2}
+      selectionType="range"
+      minimumDate={new Date()}
+      stateDefinitions={stateDefinitions}
+      dateStates={dateRanges}
+      defaultState="available"
+      showLegend={true}
+      value={value}
+      onSelect={handleSelect}
+    />
+  );
+};
--- src/pages/DetailpPages/DetailInfo/DetailEquipment/DetailEquipment.tsx
+++ src/pages/DetailpPages/DetailInfo/DetailEquipment/DetailEquipment.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { AiFillCar, AiOutlineWifi } from "react-icons/Ai";
 import { TbCurrencyNaira, TbToolsKitchen2 } from "react-icons/Tb";
-import { IoIosSnow } from "react-icons/Io";
-import { SlScreenDesktop } from "react-icons/Sl";
-import {GiBackpack} from "react-icons/Gi"
-import { MdOutlineYard } from "react-icons/md";
 
 type Props = {};
 
@@ -14,11 +10,11 @@ export default function DetailEquipment({}: Props) {
       <div className="font-medium text-xl">Nơi này có những gì cho bạn</div>
       <div className="flex items-center gap-4">
         <div className="text-base">
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             <TbToolsKitchen2 />
             <p>Bếp</p>
           </div>
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             <AiFillCar />
             <p>Chỗ đỗ xe miễn phí tại nơi ở</p>
           </div>
@@ -26,35 +22,35 @@ export default function DetailEquipment({}: Props) {
             {/* <IoIosSnow /> */}
             <p>Máy điều hòa không khí gắn trên cửa sổ</p>
           </div>
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             {/* <MdOutlineYard /> */}
             <p>Sân sau riêng</p>
           </div>
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             {/* <MdDryCleaning /> */}
             <p>Máy sấy tóc</p>
           </div>
         </div>
         <div className="text-base">
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             <AiOutlineWifi />
             <p>Wi-fi</p>
           </div>
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             {/* <SlScreenDesktop /> */}
             <p>HDTV với Netflix</p>
           </div>
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             {/* <MdOutlineYard /> */}
             <p>Sân hoặc ban công</p>
           </div>
-          <div className="flex items-center  gap-2">
+          <div className="flex items-center gap-2">
             {/* <GiBackpack /> */}
             <p>Cho phép gửi hành lý</p>
           </div>
           <div className="flex items-center gap-2">
             {/* <TbCurrencyNaira /> */}
-            <p>Máy phát hiện khí CO</p>
+            <p className="line-through">Máy phát hiện khí CO</p>
           </div>
         </div>
       </div>
